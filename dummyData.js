30be5b00371afcf6ef959ec89fbb701cd676930c
guylil <guylilian@gmail.com>
2017-01-15 17:45:34 +0200

    last fix

--- dummyData.js
+++ dummyData.js
@@ -1,40 +1,43 @@
 'use strict'
+function insertDocuments(numOfDummyEntries, db, callback) {
 
-function insertDocuments(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('meal');
-    // Insert some documents
-    collection.insertMany([
-    { foods: 'string',  time: Date.now()+Math.random()*1000 ,  userId: 1 },
-    { foods: 'string',  time: Date.now()+Math.random()*1000 ,  userId: 2 },
-    { foods: 'string',  time: Date.now()+Math.random()*1000 ,  userId: 3 },
-    { foods: 'string',  time: Date.now()+Math.random()*1000 ,  userId: 4 },
-    { foods: 'string',  time: Date.now()+Math.random()*1000 ,  userId: 5 },
-    ]);
-    db.collection('feeling').insertMany([
-        {rating:1,time: Date.now()+Math.random()*1000,userId:1},
-        {rating:3,time: Date.now()+Math.random()*1000,userId:2},
-        {rating:5,time: Date.now()+Math.random()*1000,userId:3}
-    ]);
-    db.collection('user').insertMany([
-        {eamil:'aa@gmail.com',password: 123465, imageUrl:'www',settings:{pushTImer:1000,lnag:'en'}},
-        {eamil:'aabb@gmail.com',password: 123465, imageUrl:'www',settings:{pushTImer:1000,lnag:'en'}},
-        {eamil:'aacc@gmail.com',password: 123465, imageUrl:'www',settings:{pushTImer:1000,lnag:'en'}}
-    ]);
-}
+    for (var i = 0; i < numOfDummyEntries; i++) {
+        // Get the documents collection
+        var collection = db.collection('meal');
+        // Insert some documents
+        collection.insertMany([
+            { foods: 'string' + Math.random(), time: Date.now() + Math.random() * 1000, userId: i + 1 },
+        ]);
+    };
 
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+    for (var i = 0; i < numOfDummyEntries; i++) {
+        db.collection('feeling').insertMany([
+            { rating: Math.random() * 10, time: Date.now() + Math.random() * 1000, userId: i + 1 }
+        ]);
+    };
+    for (var i = 0; i < numOfDummyEntries; i++) {
+        db.collection('user').insertMany([
+            { eamil: 'aa@gmail.com', password: 123465, imageUrl: 'www', settings: { pushTImer: 1000, lnag: 'en' } },
+        ]);
+    };
+};
 
-// Connection URL
-var projectName = 'FoodTrack'
-var url = 'mongodb://localhost:27017/' + projectName;
-// Use connect method to connect to the server
-MongoClient.connect(url, function (err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-    insertDocuments(db, function () {
-        db.close();
+
+function connectMongo(numberOfNewEntries) {
+    var MongoClient = require('mongodb').MongoClient;
+    var assert = require('assert');
+
+    // Connection URL
+    var projectName = 'FoodTrack'
+    var url = 'mongodb://localhost:27017/' + projectName;
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        insertDocuments(numberOfNewEntries, db, function () {
+            db.close();
+        });
     });
-});
+};
 
+connectMongo(10);
